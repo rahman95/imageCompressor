1a4c0ae0bb88a18936959fd57b971c9984fb6d40
Rahman Younus <rahman_95@live.co.uk>
2018-02-18 17:25:07 +0000

    added compression method

--- src/Controllers/ImageController.php
+++ src/Controllers/ImageController.php
@@ -3,11 +3,32 @@
 namespace RYounus\ImageCompression\Controllers;
 
 use App\Http\Controllers\Controller;
+use Tinify\Tinify;
+use Illuminate\Http\UploadedFile;
+use StdClass;
 
 class ImageController extends Controller{
 
-    public function index()
+    private $tinify;
+    public function __construct(Tinify $tinify)
     {
-        return '123';
+        $this->tinify = $tinify;
     }
+
+    public function compress(UploadedFile $file)
+	{
+		try {
+		    $response = new StdClass;
+            $response->fileName = $file->getClientOriginalName();
+            $response->originalSize = $file->getClientSize();
+            $response->newSize = \Tinify\fromFile($file->getRealPath())->toFile($file->getRealPath());
+            $response->compession = round(100 - ($response->newSize / $response->originalSize) * 100,2);
+            $response->extension = $file->getClientOriginalExtension();
+            $response->file = $file;
+		} catch (\Exception $e) {
+		    throw new $e;
+        }
+
+		return $response;
+	}
 }
--- src/ImageServiceProvider.php
+++ src/ImageServiceProvider.php
@@ -5,18 +5,24 @@ namespace RYounus\ImageCompression;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\UploadedFile;
 use RYounus\ImageCompression\Controllers\ImageController;
+use Tinify\Tinify;
 
 class ImageServiceProvider extends ServiceProvider{
 
+    private $tinify;
     public function boot()
     {
-        //
+        $this->app->singleton(ImageController::class, function() {
+            $this->tinify = new Tinify;
+            $this->tinify->setKey($this->app->config['services.tinify']);
+            return new ImageController($this->tinify);
+        });
     }
 
     public function register(){
-        UploadedFile::macro('index', function() {
+        UploadedFile::macro('compress', function() {
             $image = resolve(ImageController::class);
-            return $image->index();
+            return $image->compress($this);
         });
     }
 }
