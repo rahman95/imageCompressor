86022cc48d0573991064d88750d1cae54b701d73
Rahman Younus <rahman_95@live.co.uk>
2018-02-18 15:29:18 +0000

    Initial Commit

---
+++ src/ImageServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ryounus\imageCompression;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImageServiceProvider extends ServiceProvider{
+
+    public function boot()
+    {
+        //
+    }
+
+    public function register(){
+        //
+    }
+}
